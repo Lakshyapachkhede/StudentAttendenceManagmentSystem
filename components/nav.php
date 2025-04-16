71bfaf9a6f9ff1924a274456ad16aac066c05e5a
Lakshyapachkhede <pachkhedelakshya@gmail.com>
2025-04-16 22:46:51 +0530

    Designed home page and created project structure

---
+++ components/nav.php
@@ -0,0 +1,23 @@
+		<nav class="navbar d-fcc">
+
+			<div class="nav-left d-fcc">
+				<img src="img/logo.png" alt="Logo" class="logo">
+				<p><span>Sam</span>System</p> 
+			</div>
+
+
+			<div class="nav-right d-fcc">
+				<ul class="nav-links-ul d-fcc ul-sn">
+					<li><a href="" class="nav-links">Features</a></li>
+					<li><a href="" class="nav-links">Guide</a></li>
+					<li><a href="" class="nav-links">About</a></li>
+				</ul>
+
+				<ul class="nav-buttons-ul d-fcc ul-sn">
+					<li><a href="" class="login-link">Log In</a></li>
+					<li><a href="" class="btn-o">Sign Up</a></li>
+				</ul>
+			</div>
+
+
+		</nav>
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Student Attendence Managment System</title>
+
+	<link rel="stylesheet" href="css/util.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="icon" type="image/x-icon" href="/img/logo.ico">
+</head>
+<body>
+	
+	<div class="container-l">
+
+		<?php  require 'components/nav.php';?>
+
+
+		<h1 class="title fq">Student Attendance Management System</h1>
+
+		<div class="content">
+			<div class="content-row">
+				<img src="img/home1.png">
+				<p>Attendance is one of those administrative tasks that must be done at the start of each class. It can take up valuable time at the beginning of class and sometimes be difficult to manage. Attendance books traditionally are big grids with tiny squares that are hard to read and can be easy to make errors in. With QuickSchools' Online Student Information System (also known as Online School Management System), taking and managing attendance is no longer a hassle with our easy to use and robust school attendance system.</p>
+			</div>		
+
+
+			<div class="content-row">
+				<p>Attendance is one of those administrative tasks that must be done at the start of each class. It can take up valuable time at the beginning of class and sometimes be difficult to manage. Attendance books traditionally are big grids with tiny squares that are hard to read and can be easy to make errors in. With QuickSchools' Online Student Information System (also known as Online School Management System), taking and managing attendance is no longer a hassle with our easy to use and robust school attendance system.</p>
+			</div>
+			<div class="content-row">
+			
+				<p>Online Parent Tracking</p>
+			</div>
+			<div class="content-row">
+				
+				<p>Attendance is one of those administrative tasks that must be done at the start of each class. It can take up valuable time at the beginning of class and sometimes be difficult to manage. Attendance books traditionally are big grids with tiny squares that are hard to read and can be easy to make errors in. With QuickSchools' Online Student Information System (also known as Online School Management System), taking and managing attendance is no longer a hassle with our easy to use and robust school attendance system.</p>
+				<img src="img/home2.png">
+			</div>
+			<div class="content-row">
+				<p>Easy to Read Pages</p>
+			</div>
+
+			<div class="content-row">
+				<p>Another fantastic benefit of QuickSchool’s attendance feature is that the students attendance information is available to parents immediately. Once a teacher saves the attendance record for the period, QuickSchools automatically posts the attendance information to the appropriate Parent Portal without any further action from teachers. Parents can check in with the school through the Parent Portal once a day, once a week, or even period-by-period to ensure their students are attending their classes.</p>
+			</div>
+		</div>
+
+
+
+	</div>
+			<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<div class="label">
+			<p>Experience Effortless Attendence Management with SAM System  <a href="#" class="btn-l ml20">Sign Up For Free</a></p>
+
+		</div>
+
+
+
+</body>
+</html>
